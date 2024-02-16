745d34b0f8e57c82a93318ecd534979dba96184a
hhwk <kripvan@gmail.com>
2024-02-16 17:41:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -687,8 +687,7 @@ if menu=='Посещения':
     for slash in range(0, len(db_content)):
         st.write(db_content[slash])
         st.write(db_content[slash]['key'])
-        xxx=len(db_content)
-        st.write('хух',db_content[xxx]['key'])
+        st.write(len(db_content))
         if st.button(f'Удалить запрос{slash}'):
             slash+=1
             request.delete(f'{slash}')
